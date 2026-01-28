0831f60f78efb56a01a65419adf27d64d5f13fd8
Varsha413 <varsha.srb.s@gmail.com>
2026-01-28 12:20:10 +0530

    updated

--- calculator/src/main/java/calculator/Addition.java
+++ calculator/src/main/java/calculator/Addition.java
@@ -6,7 +6,7 @@ public class Addition {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter the first number is: ");
         int number1 = scanner.nextInt();
         
         System.out.print("Enter the second number: ");
